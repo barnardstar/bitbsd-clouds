15879eea807460cd3c061123822dae26173d655e
bebebe-cmd <b@ysx.in>
2019-11-03 00:14:56 +0200

    fix tor

--- rootshell/install_tor.sh
+++ rootshell/install_tor.sh
@@ -3,6 +3,7 @@ echo "installing TOR.."
 pkg install -y tor
 echo "enabling TOR"
 sysrc tor_enable=YES
+echo "Socks5Proxy 192.168.0.199:9050" >> /usr/local/etc/tor/torrc
 echo "starting TOR"
 service tor start
 echo "done!"
