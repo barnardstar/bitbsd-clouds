eec354f80b161b5c8ca7cdcb25b2f394064a2664
bebebe-cmd <b@ysx.in>
2019-11-07 13:19:52 +0200

    add onion service

--- rootshell/install_onion_service.sh
+++ rootshell/install_onion_service.sh
@@ -3,22 +3,27 @@ import os
 import fileinput
 
 print("installing TOR..")
-os.system("pkg install -y tor rsync")
+os.system("pkg install -y tor rsync nginx")
 print("enabling TOR")
 os.system("sysrc tor_enable=YES")
+os.system("sysrc enable_enable=YES")
 os.system('echo "Socks5Proxy 192.168.0.199:9050" >> /usr/local/etc/tor/torrc')
 print("starting TOR")
 os.system("service tor start")
 print("done! now enabling TOR service")
-for line in fileinput.input("/usr/local/etc/tor/torrc", inplace = 1):
-    print(line.replace("#HiddenServiceDir /var/db/tor/other_hidden_service", "HiddenServiceDir /var/db/tor/other_hidden_service"))
-    print(line.replace("#HiddenServicePort 80 127.0.0.1:80", "HiddenServicePort 80 localhost:80"))
-
 with fileinput.FileInput("/usr/local/etc/tor/torrc", inplace=True, backup='.bak') as file:
     for line in file:
         print(line.replace("#HiddenServiceDir /var/db/tor/other_hidden_service", "HiddenServiceDir /var/db/tor/other_hidden_service"), end='')
         print(line.replace("#HiddenServicePort 80 127.0.0.1:80", "HiddenServicePort 80 localhost:80"), end='')
-
+os.system("mkdir /var/db/tor/other_hidden_service")
+os.system("chown -R _tor:_tor /var/db/tor/other_hidden_service")
+os.system("chmod -R 600 /var/db/tor/other_hidden_service")
+os.system("service tor restart")
+os.system("cd /usr/local/www/nginx && ls -la")
+print('Edit content by runnng "nano index.html"')
+print('your .onion:')
+os.system("cat /var/db/tor/other_hidden_service")
+os.system("cat /var/db/tor/other_hidden_service >> /usr/local/www/nginx/index.html")
 print("config file: /usr/local/etc/tor/torrc")
 print("to enable .onion service edit 'HiddenService' part in /usr/local/etc/tor/torrc")
 print("use 'service tor restart' to restart daemon after config changed!")
